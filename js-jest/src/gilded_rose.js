3e74291116205c90037a5c259d74e66c90bb772f
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2021-09-29 17:57:40 -0300

    feat: starting change de legacy code

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -11,6 +11,10 @@ class Shop {
 		this.items = items;
 	}
 
+	static agedBrie = "Aged Brie";
+	static backstage = "Backstage passes to a TAFKAL80ETC concert";
+	static sulfuras = "Sulfuras, Hand of Ragnaros";
+
 	get allItems() {
 		return this.items;
 	}
@@ -18,20 +22,18 @@ class Shop {
 	updateQuality() {
 		for (let i = 0; i < this.items.length; i++) {
 			if (
-				this.items[i].name != "Aged Brie" &&
-				this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
+				this.items[i].name != Shop.agedBrie &&
+				this.items[i].name != Shop.backstage
 			) {
 				if (this.items[i].quality > 0) {
-					if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+					if (this.items[i].name != Shop.sulfuras) {
 						this.items[i].quality = this.items[i].quality - 1;
 					}
 				}
 			} else {
 				if (this.items[i].quality < 50) {
 					this.items[i].quality = this.items[i].quality + 1;
-					if (
-						this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
-					) {
+					if (this.items[i].name == Shop.backstage) {
 						if (this.items[i].sellIn < 11) {
 							if (this.items[i].quality < 50) {
 								this.items[i].quality = this.items[i].quality + 1;
@@ -45,16 +47,14 @@ class Shop {
 					}
 				}
 			}
-			if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+			if (this.items[i].name != Shop.sulfuras) {
 				this.items[i].sellIn = this.items[i].sellIn - 1;
 			}
 			if (this.items[i].sellIn < 0) {
-				if (this.items[i].name != "Aged Brie") {
-					if (
-						this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-					) {
+				if (this.items[i].name != Shop.agedBrie) {
+					if (this.items[i].name != Shop.backstage) {
 						if (this.items[i].quality > 0) {
-							if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+							if (this.items[i].name != Shop.sulfuras) {
 								this.items[i].quality = this.items[i].quality - 1;
 							}
 						}
